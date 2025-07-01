369698be5fbd7a0592d44fd5881ae4659bfe3a9f
manoj-sampang <iloveump40@gmail.com>
2025-07-01 19:31:46 +0545

    update code

--- generator.py
+++ generator.py
@@ -12,4 +12,18 @@ if type(even_generator()) == types.GeneratorType:
     for _ in range(10):
         print(next(even))
 
-    
+
+def square_generator(n):
+    i = 1
+    while (i<=n):
+        yield i**2
+        i+=1
+import types
+
+x = int(input("how many terms do you want the series till: "))
+if type(square_generator(x)) == types.GeneratorType:
+    print("This is generator type")
+    print("The series of squares from 1 to %d term are given below" %(x))
+    square = square_generator(x)
+    for _ in range(x):
+        print(next(square))
